a634da66c0716510ee4f6256ed5e93e066937e7d
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-31 03:11:28 +0530

    new commit

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -31,7 +31,7 @@ export default function Random() {
     }
 
     return ( 
-        <div className="w-1/2 h-[450px] rounded-lg p-3 bg-white shadow-md flex flex-col mx-auto items-center gap-y-5 mt-[15px] shadow-[#bc6868]">
+        <div className="w-3/4 h-[450px] rounded-lg p-3 bg-white shadow-md flex flex-col mx-auto items-center gap-y-5 mt-[15px] shadow-[#bc6868]">
             <h1 className="text-2xl uppercase underline font-bold ">A Random Gif</h1>
             <img className="h-[300px]" src={gif} alt="GIF" width="450"/>
             <button className="bg-black w-[70%] p-1 rounded-lg text-white" onClick={clickhandler}>Generate GIF</button>
