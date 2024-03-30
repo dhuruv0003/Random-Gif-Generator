40071d0f1a1b8de8f5d71a0038016b71a567ecc5
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-31 03:28:07 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Random from './components/Random'
 import Tag from './components/Tag'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +13,7 @@ function App() {
       mt-10 text-3xl'>RANDOM GIFS</h1>
       <div className="flex flex-col w-full items-center justify-center">
         <Random/>
+      
         <Tag/>
       </div>
     </div>
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,39 +1,47 @@
 import axios from "axios";
-import { useEffect, useState } 
-from "react"
-
+import { useEffect, useState }
+    from "react"
+import Spinner from "./Spinner";
 
 export default function Random() {
 
-    const [gif, setgif]=useState("")
+    const [gif, setgif] = useState("")
+    const [loading, setloading] = useState(false);
 
-    const API_KEY='c8OFoOD0mNfNXwObyKIgm0Q3wwVMqDXa'   
+    const API_KEY = 'HmJrhDm1Ga38Cj26DpfZJhYHLAbCxhYj'
 
-    async function fetchData(){
+    async function fetchData() {
+        setloading(true);
         const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-            
+
         // axios.get() returns a promise 
 
-        const {data}=await axios.get(url)
+        const { data } = await axios.get(url)
         // {data} se hame api ka data wala element mil gya h
         // Ab hame uske andar se url nikalna hai 
 
-        const imgsrc=data.data.images.downsized_large.url
+        const imgsrc = data.data.images.downsized_large.url
         setgif(imgsrc)
+        setloading(false);
     }
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
-    },[])
-    
+    }, [])
 
-    function clickhandler(){
+
+    function clickhandler() {
         fetchData();
     }
 
-    return ( 
-        <div className="w-3/4 h-[450px] rounded-lg p-3 bg-white shadow-md flex flex-col mx-auto items-center gap-y-5 mt-[15px] shadow-[#bc6868]">
+    return (
+        <div className="w-3/4  rounded-lg p-3 bg-white shadow-md flex flex-col mx-auto items-center gap-y-5 mt-[15px] shadow-[#bc6868]">
             <h1 className="text-2xl uppercase underline font-bold ">A Random Gif</h1>
-            <img className="h-[300px]" src={gif} alt="GIF" width="450"/>
+
+            {
+                loading?(<Spinner/>):(<img  src={gif} alt="GIF" width="450" />)
+            }
+
+            
             <button className="bg-black w-[70%] p-1 rounded-lg text-white" onClick={clickhandler}>Generate GIF</button>
 
         </div>
