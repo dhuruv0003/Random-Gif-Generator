e02a14cf0c1a03a62cd0cf803b63c44933556baa
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-31 15:26:30 +0530

    new commit

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,5 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import Spinner from "./Spinner";
+
 export default function Tag(){
+
+    const [tag,settag]=useState("")
+    const [loading, setloading]=useState(false)
+    const[gif,setgif]=useState("")
+
+    const API_KEY = 'olMAEV8K4xiKmqapN9OLDPGghNj3nSxX'
+
+    async function fetchData(){
+        setloading(true);
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        const {data}=await axios.get(url);
+        const imgsrc = data.data.images.downsized_large.url
+        setgif(imgsrc)
+        setloading(false);
+    }
+
+useEffect(()=>{
+    fetchData();
+},[]);
+
+
     return(
-        <div className=""></div>
+        <div className="w-3/4  rounded-lg p-3 bg-white shadow-md flex flex-col mx-auto items-center gap-y-5 mt-[15px] shadow-[#bc6868]">
+            
+            <h1>Random Gif</h1>
+            
+            {
+                loading?(<Spinner/>):(<img  src={gif} alt="GIF" width="450" />)
+            }
+            <button onClick={()=>fetchData()}>Generate</button>
+        </div>
     )
 }
